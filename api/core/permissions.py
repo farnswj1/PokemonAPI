39023a3189ed02aa737aebbdb047c46861107465
Justin Farnsworth <fjj12697@gmail.com>
2024-11-21 17:31:58 -0500

    add type hints

--- api/core/permissions.py
+++ api/core/permissions.py
@@ -1,6 +1,8 @@
+from django.http.request import HttpRequest
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.views import APIView
 
 
 class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
+    def has_permission(self, request: HttpRequest, view: APIView):
         return request.method in SAFE_METHODS or request.user.is_staff
--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import Token
+from core.models import User
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
-    def get_token(cls, user):
+    def get_token(cls, user: User) -> Token:
         token = super().get_token(user)
         token['is_staff'] = user.is_staff
         return token
